6f2d8adca3e552c654f055131db49005545ecd17
CarlosCastano33 <126597422+CarlosCastano33@users.noreply.github.com>
2024-03-09 21:46:16 -0500

    Leyendo líneas del final al inicio (main)

--- reverse-Carlos-v.c
+++ reverse-Carlos-v.c
@@ -59,12 +59,68 @@ void validarArchivosDiferentesHL(char *argv[]){
 
 
 int main(int argc, char *argv[]) {
+//    validarCantidadArgumentos(argc, argv);
+  //  validarExistenciaArchivo(argc, argv);
+    //validarArchivosDiferentes(argv);
 
-    validarCantidadArgumentos(argc, argv);
+    FILE *archivo1, *archivo2;
+    char *linea1 = NULL, *linea2 = NULL;
+    size_t longitud1 = 0, longitud2 = 0;
+    ssize_t bytes_leidos1, bytes_leidos2;
+    int contador1 = 0, contador2 = 0;
 
-    validarExistenciaArchivo(argc, argv);
-    
-    validarArchivosDiferentes(argv);
+    archivo1 = fopen("in.txt", "r");
+    archivo2 = fopen("out.txt", "r");
+
+    if (archivo2 == NULL) {
+        printf(stderr, "Error al abrir el archivo");
+        return 1;
+    }
+    // Leer línea por línea de ambos archivos
+    //while (((bytes_leidos1 = getline(&linea1, &longitud1, archivo1)) != -1) && ((bytes_leidos2 = getline(&linea2, &longitud2, archivo2)) != -1)) {
+      //  contador1++; // Incrementar contador del archivo 1
+        //contador2++; // Incrementar contador del archivo 2
+    //}
+
+     // Mover el puntero de archivo al final del archivo
+    long posicion, posicion_inicial;
+    fseek(archivo2, 0, SEEK_END);
+    posicion = ftell(archivo2);
+
+    // Bucle para recorrer el archivo línea por línea
+    //posicion >= 0
+    //------------- TO DO EN VEZ DE i, PONER CANTIDAD DE LINEAS CALCULADAS-----------
+    int i = 0;
+    while (i < 4) {
+        char c;
+        // Retroceder la posición del puntero en el archivo
+        fseek(archivo2, --posicion, SEEK_SET);
+        c = fgetc(archivo2);
+
+        if (c == '\n' || posicion == 0) { // Si encontramos un salto de línea
+            if(posicion == 0){
+                rewind(archivo2);
+            }
+            getline(&linea2, &longitud2, archivo2); // Leemos la línea utilizando getline
+            printf("Línea leída: %s", linea2);
+            i++;
+        }
+    }
+
+
+
+// Mostrar el número de líneas contadas en cada archivo
+   // printf("El archivo1 tiene %d lineas.\n", contador1);
+    //printf("El archivo2 tiene %d lineas.\n", contador2);
+
+    // Liberar la memoria asignada a las líneas
+    free(linea1);
+    free(linea2);
+
+    // Cerrar los archivos
+    fclose(archivo1);
+    fclose(archivo2);
+
 
     return 0;
 }
